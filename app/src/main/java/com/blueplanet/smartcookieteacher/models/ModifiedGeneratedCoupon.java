326b15787f66b4f77e72f2c75edfc923977c32c3
Prashant Jadhav <prashantj@roseland.com>
2018-03-09 12:53:12 +0530

    Merge the priyanka

---
+++ app/src/main/java/com/blueplanet/smartcookieteacher/models/ModifiedGeneratedCoupon.java
@@ -0,0 +1,29 @@
+package com.blueplanet.smartcookieteacher.models;
+
+/**
+ * Created by prashantj on 3/8/2018.
+ */
+
+public class ModifiedGeneratedCoupon {
+
+    String couponPoints;
+    String couponId;
+
+
+
+    public String getCouponPoints() {
+        return couponPoints;
+    }
+
+    public String getCouponId() {
+        return couponId;
+    }
+
+    public void setCouponPoints(String couponPoints) {
+        this.couponPoints = couponPoints;
+    }
+
+    public void setCouponId(String couponId) {
+        this.couponId = couponId;
+    }
+}
